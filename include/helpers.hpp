fbc68176c287dfd4fb8f70a66105779196736fb7
luisrslz <luisrslz.git@gmail.com>
2026-01-12 20:41:07 +0100

    removed declaration of unused function

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -27,9 +27,6 @@ void printLogo();
 // Clears the screen and makes Windows-Terminal compatible
 void clearScreen();
 
-// Self explanatory
-void askPlayerCount(unsigned int &playerCount);
-
 // creates the players, performs initial draw and connects them to the game
 std::vector<Player> createPlayers(unsigned int playerCount, Game &game);
 
